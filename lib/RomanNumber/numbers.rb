d0797b6ceb2d7c58d40a050529485d0fbadf961a
Jochen Fromm <Jochen.Fromm@td-berlin.com>
2015-02-23 21:25:23 +0100

    comments added

--- lib/RomanNumber/numbers.rb
+++ lib/RomanNumber/numbers.rb
@@ -1,10 +1,10 @@
 module RomanNumber
   NUMBERS = [
-    ["M"  , 1000],
+    ["M"  , 1000], # M stands for mille, the Latin word for 1000.
     ["CM" , 900],
     ["D"  , 500],
     ["CD" , 400],
-    ["C"  , 100],
+    ["C"  , 100], # C stands for centum, the Latin word for 100. A centurion led 100 men, and a Century has 100 years
     ["XC" ,  90],
     ["L"  ,  50],
     ["XL" ,  40],
